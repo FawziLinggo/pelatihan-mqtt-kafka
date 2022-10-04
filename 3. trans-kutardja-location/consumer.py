d17946f29164fb3c37616dd5129549b8fec009c8
FawziLinggo <fawzilinggo@gmail.com>
2022-10-04 20:42:04 +0700

    delete

--- 3. trans-kutardja-location/consumer.py
+++
@@ -1,8 +0,0 @@
-from confluent_kafka import Consumer
-
-conf = {'bootstrap.servers': "neo4j:9092",
-        'group.id': "foo",
-        'enable.auto.commit': False,
-        'auto.offset.reset': 'earliest'}
-
-consumer = Consumer(conf)
